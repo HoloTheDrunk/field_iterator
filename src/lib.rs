04f6475a3cd7723e42d6d03889dc644ebfbe73e6
Raphaël Duhen <raphael.duhen@ign.fr>
2024-04-10 18:17:32 +0200

    refactor: move trait to main crate, remove sub

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
 pub use field_iterator_derive::Iterable;
-pub use field_iterator_trait::Iterable;
+pub trait Iterable {
+    fn iter<'a>(&'a self) -> std::vec::IntoIter<(&'static str, &'a dyn std::any::Any)>;
+    fn iter_mut<'a>(&'a mut self) -> std::vec::IntoIter<(&'static str, &'a mut dyn std::any::Any)>;
+}
--- trait/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub trait Iterable {
-    fn iter<'a>(&'a self) -> std::vec::IntoIter<(&'static str, &'a dyn std::any::Any)>;
-    fn iter_mut<'a>(&'a mut self) -> std::vec::IntoIter<(&'static str, &'a mut dyn std::any::Any)>;
-}
